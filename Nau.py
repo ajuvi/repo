dd892ae3e821d372c1bfe9f88762e8b58a90c2d1
ajuvi <ajuve@institutmontilivi.cat>
2022-04-13 01:14:53 +0200

    conflicte

--- Nau.py
+++ Nau.py
@@ -6,16 +6,16 @@ class Nau:
         self.v=1
     
     def moure_dreta(self):
-        self.x += self.v
+        self.x+=self.v
         
     def moure_esquerre(self):
-        self.x -= self.v
+        self.x-=self.v
 
     def moure_amunt(self):
-        self.y += self.v
+        self.y+=self.v
         
     def moure_avall(self):
-        self.y -= self.v
+        self.y-=self.v
         
     def mostrar_nau(self):
         print(f"({self.x},{self.y})")
