bb78937ead527503ebff6621224de2d5de8f61d5
ajuvi <ajuve@institutmontilivi.cat>
2022-04-12 22:53:43 +0200

    joc de naus

---
+++ Main.py
@@ -0,0 +1,31 @@
+from Nau import Nau
+
+def menu():
+    print("w- Moure dreta")
+    print("a- Moure equerre")
+    print("d- Moure amunt")
+    print("s- Moure moure avall")    
+    print("0- Sortir")
+
+def main():
+
+    menu()
+    
+    nau = Nau(0,0)
+
+    sortir=False
+    while not sortir:
+        op = input('Entra una opció')
+        if op=='d':
+            nau.moure_dreta()
+        elif op=='a':
+            nau.moure_esquerre()
+        elif op=='w':
+            nau.moure_amunt()
+        elif op=='s':
+            nau.moure_avall()
+        elif op=='0':
+            sortir=True
+            print("Has sortit de l'avió")
+        
+main()
